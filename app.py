bdd1aa4854ac61f41b88d56df67580d652e8298d
kaledmhd1 <Isameryem211@gmail.com>
2025-08-03 17:24:54 +0100

    app.py

--- app.py
+++ app.py
@@ -863,19 +863,17 @@ def send_likes():
         status, content = FOX_RequestAddingFriend(token, target_id)
 
         if status == 200 and isinstance(content, dict):
-            stats = content.get("stats", {})
-            if stats.get("success", 0) == 1:
-                success_count += 1
-                successful_uids.append(uid)
-                if success_count >= 60:
-                    stop_flag.set()
-            elif stats.get("daily_limited_reached", 0) == 1:
-                skipped_count += 1
-                add_to_skipped(uid)
-            else:
-                failed_count += 1
-        else:
-            failed_count += 1
+    stats = content.get("stats", {})
+    if stats.get("daily_limited_reached", 0) == 1:
+        skipped_count += 1
+        add_to_skipped(uid)
+    elif stats.get("error", 1) == 0:
+        success_count += 1
+        successful_uids.append(uid)
+        if success_count >= 60:
+            stop_flag.set()
+    else:
+        failed_count += 1
 
     with ThreadPoolExecutor(max_workers=MAX_PARALLEL_REQUESTS) as executor:
         futures = [executor.submit(process, uid, token) for uid, token in tokens_to_use.items()]
